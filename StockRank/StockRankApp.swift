d60d787bc316a213e7675520037518abc584d200
pearl <nane030@naver.com>
2023-05-07 11:32:18 +0900

    Initial Commit

---
+++ StockRank/StockRankApp.swift
@@ -0,0 +1,17 @@
+//
+//  StockRankApp.swift
+//  StockRank
+//
+//  Created by 김진주 on 2023/05/07.
+//
+
+import SwiftUI
+
+@main
+struct StockRankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
